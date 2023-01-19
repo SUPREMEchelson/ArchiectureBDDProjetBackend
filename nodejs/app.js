8470ee4f4c72eac2c7c847b32300bf8ebf2b98b6
SUPREMEchelson <chelson.sm@gmail.com>
2023-01-19 12:14:29 +0100

    CRUD

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,11 +8,82 @@ async function main(){
     console.log('Connection OK!');
     const db = client.db('myTask');
     const collection =db.collection('documents');
-    const insertStuff = await collection.insertMany([{a:1},{b:2},{c:3}])
-    console.log(`Documents insérés => ${insertStuff} `);
-    return 'done!';
-     
+    
+    // //Create 
+    // try{
+    //     const insertData = await collection.insertMany([
+    //         {
+    //             name: 'Alex',
+    //             age: 30,
+    //             sexe: 'Masculin',
+    //             hobby: 'Coding',
+    //         },
+    //         {
+    //             name: 'Justine',
+    //             age:30,
+    //             sexe: 'Féminin',
+    //             hobby: 'Coding'
+    //         },
+    //         {
+    //             name: 'Pierre',
+    //             age: 35,
+    //             sexe: 'Masculin',
+    //             hobby: 'Escalade'
+    //         }
+    //     ])
+    //     console.log('Documents insérés =>', insertData)
+    // }catch(e) {throw e;}
+
+
+// Read 
+try{
+    const findData = await collection.findOne({name: 'Justine'});
+    console.log('Document trouvé:', findData); 
+} catch(e){ throw e;}
+
+// // Read Many
+// try{
+//     const findMultipleData = await collection.find({age: 30});
+//     console.log(await findMultipleData.toArray()); 
+// } catch(e){ throw e;}
+
+// Update
+
+// try{
+//     const updatePierre = collection.updateOne({name: 'Pierre'}, {
+//         $set: {name: 'Pauline', sexe: 'Féminin'}
+
+//     });
+//     console.log(await updatePierre);
+// } catch(e) {throw e;}
+
+
+// Update many
+// try{
+// const updateAge = collection.updateMany({age: 30 }, {
+//     $set: { age: 31}
+// });
+// console.log(await updateAge);
+
+// } catch(e) {throw e;}
+
+// // Delete
+// try{
+//     const deletePierre = await collection.deleteOne({ name: 'Pauline'});
+//     console.log(await deletePierre);
+// }catch(e) {throw e};
+
+
+
+// // Delete Many
+// try{
+//     const deleteEveryone = await collection.deleteMany({age: 31});
+//     console.log(await deleteEveryone);
+// } catch(e) {throw e;}
+return 'done';
 }
+
+
 
 main()
     .then(console.log)
